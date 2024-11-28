b744db37a7e590c7b0b4f45ec109033437008d1a
Muhammad Saim <141933915+saimawan185@users.noreply.github.com>
2024-11-29 02:32:08 +0500

    Complete

--- app/src/main/java/com/example/cricketscoringapp/MatchScreen.java
+++ app/src/main/java/com/example/cricketscoringapp/MatchScreen.java
@@ -1,345 +1,3 @@
-//package com.example.cricketscoringapp;
-//
-//import android.Manifest;
-//import android.app.AlertDialog;
-//import android.content.Intent;
-//import android.content.pm.PackageManager;
-//import android.graphics.drawable.Drawable;
-//import android.os.Build;
-//import android.os.Bundle;
-//import android.view.MenuItem;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.appcompat.widget.Toolbar;
-//import androidx.core.app.ActivityCompat;
-//import androidx.core.graphics.drawable.DrawableCompat;
-//
-//import java.text.DecimalFormat;
-//import java.util.ArrayList;
-//
-//public class MatchScreen extends AppCompatActivity {
-//
-//    private TextView latitude, longitude, altitude;
-//    private ImageView muteUnmuteIcon;
-//    private ArrayList<PlayerModel> firstTeamPlayers, secondTeamPlayers, team1Ballers, team2Ballers;
-//
-//    private Button one, two, three, four, five, six, zero, noBall, wicket, wide, bies;
-//
-//    private TextView playerOneNameText, playerTwoNameText, playerOneRunsText, playerTwoRunsText, ballerNameText, ballerOverviewText,
-//            totalRunsText, totalWicketsText, totalOversText;
-//    private int totalRuns=0, totalWickets=0, team1TotalRuns=0, team2TotalRuns=0;
-//    private double overs=0.0, ballerOvers=0.0;
-//
-//    private String playerOneName,playerTwoName, ballerName;
-//    private int playerOneRuns=0, playerTwoRuns=0, ballerWickets=0, ballerRuns=0;
-//
-//    private TrackGPS gps;
-//    private boolean firstTeamPlayed = false;
-//    private int playingTeam, currentBallerIndex=0, currentPlayerIndex=1, numberOfOvers;
-//
-//    private static final int REQUEST_CODE_PERMISSION = 1;
-//    String[] mPermission = {Manifest.permission.ACCESS_FINE_LOCATION};
-//
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.match_screen);
-//
-//        firstTeamPlayers = getIntent().getParcelableArrayListExtra("firstTeamPlayers");
-//        secondTeamPlayers = getIntent().getParcelableArrayListExtra("secondTeamPlayers");
-//        numberOfOvers = Integer.valueOf(getIntent().getStringExtra("numberOfOvers"));
-//        playingTeam = getIntent().getIntExtra("teamWon", 1);
-//
-//        team1Ballers = new ArrayList<PlayerModel>();
-//        team2Ballers = new ArrayList<PlayerModel>();
-//
-//        for(var player: firstTeamPlayers)
-//        {
-//            if(player.isBaller())
-//            {
-//                team1Ballers.add(player);
-//            }
-//        }
-//
-//        for(var player: secondTeamPlayers)
-//        {
-//            if(player.isBaller())
-//            {
-//                team2Ballers.add(player);
-//            }
-//        }
-//
-//        initAll();
-//
-//        setTeams();
-//
-//        setFields();
-//
-//        MusicManager.startMusic(this);
-//
-//        oneClick();
-//
-//        muteUnmuteIcon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (MusicManager.isMusicPlaying()) {
-//                    MusicManager.pauseMusic();
-//                    muteUnmuteIcon.setImageResource(R.drawable.mute);
-//                } else {
-//                    MusicManager.startMusic(MatchScreen.this);
-//                    muteUnmuteIcon.setImageResource(R.drawable.unmute);
-//                }
-//            }
-//        });
-//
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//
-//        Drawable backIcon = getResources().getDrawable(R.drawable.back_button); // Ensure you have a back icon in drawable
-//        DrawableCompat.setTint(backIcon, getResources().getColor(android.R.color.white));  // Set the color to white
-//        toolbar.setNavigationIcon(backIcon);
-//
-//        if(Build.VERSION.SDK_INT>= 23) {
-//
-//            if (checkSelfPermission(mPermission[0]) != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(MatchScreen.this, mPermission, REQUEST_CODE_PERMISSION);
-//                return;
-//            }
-//            else {
-//                initializeGps();
-//            }
-//        }
-//        else {
-//            initializeGps();
-//        }
-//    }
-//
-//    private void oneClick()
-//    {
-//        one.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                playerOneRuns++;
-//                totalRuns++;
-//                ballerRuns++;
-//
-//                ballPlay();;
-//                swapPlayers();
-//                checkOverComplete();
-//                setFields();
-//            }
-//        });
-//    }
-//
-//    private void setTeams()
-//    {
-//        if(playingTeam==1)
-//        {
-//            playerOneName = firstTeamPlayers.get(0).getName();
-//            playerTwoName = firstTeamPlayers.get(currentPlayerIndex).getName();
-//            ballerName = team2Ballers.get(currentBallerIndex).getName();
-//        }
-//        else
-//        {
-//            playerOneName = secondTeamPlayers.get(0).getName();
-//            playerTwoName = secondTeamPlayers.get(currentPlayerIndex).getName();
-//            ballerName = team1Ballers.get(currentBallerIndex).getName();
-//        }
-//    }
-//
-//    private void ballPlay()
-//    {
-//        DecimalFormat decimalFormat = new DecimalFormat("#.#");
-//        ballerOvers = ballerOvers+0.1;
-//        overs=overs+0.1;
-//      ballerOvers=  Double.parseDouble(decimalFormat.format(ballerOvers));
-//        overs=  Double.parseDouble(decimalFormat.format(overs));
-//
-//        if(String.valueOf(ballerOvers).contains(".6"))
-//        {
-//            ballerOvers= Math.ceil(ballerOvers) ;
-//            overs = Math.ceil(overs);
-//            swapPlayers();
-//        }
-//    }
-//
-//    private  void swapPlayers()
-//    {
-//        String player = playerOneName;
-//        int runs = playerOneRuns;
-//        playerOneName = playerTwoName;
-//        playerTwoName= player;
-//        playerOneRuns = playerTwoRuns;
-//        playerTwoRuns = runs;
-//    }
-//
-//    private void checkOverComplete()
-//    {
-//        if(overs==Double.valueOf(numberOfOvers))
-//        {
-//            if(firstTeamPlayed)
-//            {
-//                if(playingTeam==1)
-//                {
-//                    if(team2TotalRuns>totalRuns)
-//                    {
-//                        showDialogBox("Success","Team 2 Won!");
-//                    }
-//                    else if(team2TotalRuns<totalRuns) {
-//                        showDialogBox("Success","Team 1 Won!");
-//                    }
-//                    else {
-//                        showDialogBox("Success","Match Withdrawn!");
-//                    }
-//                }
-//                else {
-//                    if(team1TotalRuns>totalRuns)
-//                    {
-//                        showDialogBox("Success","Team 1 Won!");
-//                    }
-//                    else if(team1TotalRuns<totalRuns) {
-//                        showDialogBox("Success","Team 2 Won!");
-//                    }
-//                    else {
-//                        showDialogBox("Success","Match WWithdrawn!");
-//                    }
-//                }
-//            }
-//            else {
-//                if(playingTeam==1)
-//                {
-//                    team1TotalRuns = totalRuns;
-//                    playingTeam =2;
-//                }
-//                else {
-//                    team2TotalRuns = totalRuns;
-//                    playingTeam =1;
-//                }
-//                showDialogBox("","Total Runs: "+totalRuns);
-//                firstTeamPlayed=true;
-//                totalRuns =0;
-//                currentBallerIndex = 0;
-//                currentPlayerIndex=1;
-//                playerOneRuns =0;
-//                playerTwoRuns =0;
-//                ballerOvers=0.0;
-//                overs=0.0;
-//                ballerRuns=0;
-//                ballerWickets=0;
-//                totalWickets=0;
-//                setTeams();
-//                setFields();
-//            }
-//        }
-//    }
-//
-//    @Override
-//    protected void onDestroy(){
-//        super.onDestroy();
-//        gps.stopGPS();
-//    }
-//
-//    private void setFields()
-//    {
-//        playerOneNameText.setText(playerOneName);
-//        playerOneRunsText.setText(String.valueOf(playerOneRuns));
-//        playerTwoNameText.setText(playerTwoName);
-//        playerTwoRunsText.setText(String.valueOf(playerTwoRuns));
-//        ballerNameText.setText(ballerName);
-//        ballerOverviewText.setText(String.valueOf(ballerOvers)+" ("+String.valueOf(ballerRuns)+") "+String.valueOf(ballerWickets));
-//        totalRunsText.setText(String.valueOf(totalRuns));
-//        totalWicketsText.setText(String.valueOf(totalWickets));
-//        totalOversText.setText(String.valueOf(overs));
-//    }
-//
-//    private void initAll()
-//    {
-//        latitude = findViewById(R.id.latitude);
-//        longitude = findViewById(R.id.longitude);
-//        altitude = findViewById(R.id.altitude);
-//        muteUnmuteIcon = findViewById(R.id.muteUnmuteIcon);
-//
-//        one = findViewById(R.id.one);
-//        two = findViewById(R.id.two);
-//        three = findViewById(R.id.three);
-//        four = findViewById(R.id.four);
-//        five = findViewById(R.id.five);
-//        six = findViewById(R.id.six);
-//        zero = findViewById(R.id.zero);
-//        noBall = findViewById(R.id.noBall);
-//        wicket = findViewById(R.id.wicket);
-//        wide = findViewById(R.id.wide);
-//        bies = findViewById(R.id.bies);
-//        playerOneNameText = findViewById(R.id.player1Name);
-//        playerOneRunsText = findViewById(R.id.player1Runs);
-//        playerTwoNameText = findViewById(R.id.player2Name);
-//        playerTwoRunsText = findViewById(R.id.player2Runs);
-//        ballerNameText = findViewById(R.id.ballerName);
-//        ballerOverviewText = findViewById(R.id.ballerOverview);
-//        totalRunsText = findViewById(R.id.runs);
-//        totalWicketsText = findViewById(R.id.wickets);
-//        totalOversText = findViewById(R.id.overs);
-//    }
-//
-//    private void showDialogBox(String title, String description) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setTitle(title); // Set the dialog title
-//        builder.setMessage(description); // Set the dialog message
-//
-//        builder.setPositiveButton("OK", (dialog, which) -> {
-//            dialog.dismiss();
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-//    }
-//
-//    private void initializeGps()
-//    {
-//        gps=new TrackGPS(MatchScreen.this);
-//        if (gps.canGetLocation()){
-//            latitude.setText("Latitude: "+gps.getLatitude());
-//            longitude.setText("Longitude: "+gps.getLongitude());
-//            altitude.setText("Altitude: "+gps.getAltitude());
-//        }
-//        else{
-//            gps.showAlert();
-//        }
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        if (requestCode == REQUEST_CODE_PERMISSION) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                initializeGps();
-//            } else {
-//                Toast.makeText(this, "Permission denied, cannot get location", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == android.R.id.home) {
-//            onBackPressed();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-//}
-
-
 package com.example.cricketscoringapp;
 
 import android.Manifest;
@@ -374,7 +32,7 @@ public class MatchScreen extends AppCompatActivity {
 
     private TextView playerOneNameText, playerTwoNameText, playerOneRunsText, playerTwoRunsText, ballerNameText, ballerOverviewText,
             totalRunsText, totalWicketsText, totalOversText;
-    private int totalRuns = 0, totalWickets = 0, team1TotalRuns = 0, team2TotalRuns = 0;
+    private int totalRuns = 0, totalWickets = 0, team1TotalRuns = 0, team2TotalRuns = 0, targetScore;
     private double overs = 0.0, ballerOvers = 0.0;
 
     private String playerOneName, playerTwoName, ballerName;
@@ -416,7 +74,6 @@ public class MatchScreen extends AppCompatActivity {
         setTeams();
         setFields();
 
-        // Music Manager example
         MusicManager.startMusic(this);
 
         muteUnmuteIcon.setOnClickListener(v -> {
@@ -560,6 +217,7 @@ public class MatchScreen extends AppCompatActivity {
         noBall.setOnClickListener(v -> {
             totalRuns++;
             ballerRuns++;
+            checkTargetAchieved();
             setFields();
         });
     }
@@ -568,11 +226,12 @@ public class MatchScreen extends AppCompatActivity {
         wicket.setOnClickListener(v -> {
             totalWickets++;
             ballerWickets++;
+            ballPlay();
             currentPlayerIndex++;
             if (currentPlayerIndex >= firstTeamPlayers.size()) {
                 showDialogBox("Innings Over", "All players are out!");
             } else {
-                playerTwoName = firstTeamPlayers.get(currentPlayerIndex).getName();
+                playerOneName = firstTeamPlayers.get(currentPlayerIndex).getName();
             }
             checkOverComplete();
             setFields();
@@ -583,6 +242,7 @@ public class MatchScreen extends AppCompatActivity {
         wide.setOnClickListener(v -> {
             totalRuns++;
             ballerRuns++;
+            checkTargetAchieved();
             setFields();
         });
     }
@@ -603,6 +263,7 @@ public class MatchScreen extends AppCompatActivity {
         if (runs % 2 != 0) {
             swapPlayers();
         }
+        checkTargetAchieved();
         checkOverComplete();
         setFields();
     }
@@ -621,25 +282,55 @@ public class MatchScreen extends AppCompatActivity {
 
     private void checkOverComplete() {
         if (overs == numberOfOvers || totalWickets >= firstTeamPlayers.size()) {
-            if (firstTeamPlayed) {
-                showMatchResult(); // Show result when the second team finishes
-            } else {
+            if (!firstTeamPlayed) {
                 firstTeamPlayed = true;
-                saveFirstInningsData(); // Save first innings data
-                resetForSecondInnings();
+                saveFirstInningsData();
+
+                new AlertDialog.Builder(MatchScreen.this)
+                        .setTitle("First Innings Over")
+                        .setMessage("Team " + playingTeam + " scored " + totalRuns + " runs!")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            dialog.dismiss();
+                            resetForSecondInnings();
+                        })
+                        .show();
+            } else {
+                showMatchResult();
             }
         }
     }
+
     private void saveFirstInningsData() {
+
         if (playingTeam == 1) {
             team1TotalRuns = totalRuns;
-            targetScoreText.setText("Target: " + (team1TotalRuns + 1));
+            targetScore = team1TotalRuns + 1;
+            targetScoreText.setText("Target: " + targetScore);
         } else {
             team2TotalRuns = totalRuns;
-            targetScoreText.setText("Target: " + (team2TotalRuns + 1));
+            targetScore = team2TotalRuns + 1;
+            targetScoreText.setText("Target: " + targetScore);
         }
     }
 
+    private void checkTargetAchieved() {
+        if (firstTeamPlayed) {
+            if (totalRuns >= targetScore) {
+                showDialogBox("Match Over", "Team 2 Won!");
+            }
+        } else {
+            if (playingTeam == 2 && totalRuns >= targetScore) {
+                showDialogBox("Match Over", "Team 1 Won!");
+            }
+        }
+    }
+
+    private void navigateToMain() {
+        Intent intent = new Intent(MatchScreen.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
 
     private void showMatchResult() {
         String resultMessage;
@@ -698,7 +389,6 @@ public class MatchScreen extends AppCompatActivity {
             ballerName = team1Ballers.get(currentBallerIndex).getName();
         }
     }
-
     private void setFields() {
         playerOneNameText.setText(playerOneName);
         playerOneRunsText.setText(String.valueOf(playerOneRuns));
